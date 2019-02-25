8e53800282f78aecc57cd9b8e6919e16ad2ad1dd
raffavivians <raffavivians@gmail.com>
2019-02-25 15:09:07 +0100

    Updates

---
+++ precache-manifest.76f262a01854e2ae46ab6d52532b14e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c3f209f6482ab958fde8a024ffe2d90",
+    "url": "/tabella/static/media/logo_B2.0c3f209f.png"
+  },
+  {
+    "revision": "d01a84880453d7e13b3d",
+    "url": "/tabella/static/js/runtime~main.d01a8488.js"
+  },
+  {
+    "revision": "8ab7dfc36b354f71fb24",
+    "url": "/tabella/static/js/main.8ab7dfc3.chunk.js"
+  },
+  {
+    "revision": "f87bb752134df4c546a6",
+    "url": "/tabella/static/js/1.f87bb752.chunk.js"
+  },
+  {
+    "revision": "8ab7dfc36b354f71fb24",
+    "url": "/tabella/static/css/main.971537d9.chunk.css"
+  },
+  {
+    "revision": "a0f0eea74336b4782350d6531a6d7f3d",
+    "url": "/tabella/index.html"
+  }
+];
